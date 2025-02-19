5a81c025218e3abae37bfd9d0633f2f6f6e054c2
Chalegre12 <144504092+Chalegre12@users.noreply.github.com>
2025-02-19 11:01:06 -0300

    Add files via upload

---
+++ calculadora.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+void main() {
+  print('Digite o primeiro número:');
+  double num1 = double.parse(stdin.readLineSync()!);
+
+  print('Digite o segundo número:');
+  double num2 = double.parse(stdin.readLineSync()!);
+
+  print('Escolha a operação (+, -, *, /):');
+  String operacao = stdin.readLineSync()!;
+
+  double resultado = (operacao == '+') 
+      ? num1 + num2 
+      : (operacao == '-') 
+      ? num1 - num2 
+      : (operacao == '*') 
+      ? num1 * num2 
+      : (operacao == '/' && num2 != 0) 
+      ? num1 / num2 
+      : throw Exception('Operação inválida ou divisão por zero');
+
+  print('O resultado é: $resultado');
+}
